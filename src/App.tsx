04a0b38d446cedc3de7ef0b58f0070589c0afe38
Amilto Romagno <amiltoromagno@outlook.com>
2024-11-12 17:46:13 -0300

    chore: add github icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
+import { FaGithub } from 'react-icons/fa6'
 import './App.css'
 import Home from './components/Home'
 
 function App () {
   return (
-    <div className='h-full w-full bg-[url("/src/assets/background.png")] bg-cover bg-center bg-no-repeat'>
+    <div className='h-full w-full bg-[url("/src/assets/background.png")] bg-cover bg-center bg-no-repeat relative'>
+      <a
+        href='https://github.com/amiltoromagno/pokedex-extension-app'
+        target='_blank'
+        rel='noopener noreferrer'
+        className='absolute bottom-4 right-4 text-black text-2xl'
+      >
+        <FaGithub />
+      </a>
       <div className='h-full w-ful flex justify-center items-center'>
         <Home />
       </div>
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,13 +3,15 @@ import Search from './Search'
 const Home = () => {
   return (
     <div className='w-4/5 flex justify-center items-center flex-col'>
-        <div className='w-100% h-20 flex'>
-          <img src='pokeball-logo.png' alt='logo' />
-          <div className='text-black text-5xl flex items-center ml-2'>Pokédex</div>
+      <div className='w-100% h-20 flex'>
+        <img src='pokeball-logo.png' alt='logo' />
+        <div className='text-black text-5xl flex items-center ml-2'>
+          Pokédex
         </div>
-        <div>
-          <Search />
-        </div>
+      </div>
+      <div>
+        <Search />
+      </div>
     </div>
   )
 }
