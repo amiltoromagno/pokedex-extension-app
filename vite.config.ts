8511b5af72d6267c1e32c8e0a357c6c1e2ccbcef
Amilto Romagno <amiltoromagno@outlook.com>
2024-11-13 20:11:09 -0300

    chore: config setup and readme update

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,26 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import tailwindcss from "tailwindcss";
+import tailwindcss from 'tailwindcss'
 
 export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
       input: {
-        main: 'index.html'
+        main: 'index.html',
+        content: 'src/main.tsx'
+      },
+      output: {
+        entryFileNames: '[name].js',
+        assetFileNames: '[name].[ext]'
       }
     },
-    outDir: 'dist'
+    outDir: 'dist',
+    manifest: false
   },
   css: {
     postcss: {
-      plugins: [tailwindcss()],
-    },
-  },
+      plugins: [tailwindcss()]
+    }
+  }
 })
